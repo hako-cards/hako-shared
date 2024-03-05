9d48ab8b069da62116b9c4e05c0cae0c2266f3bd
Kyle Bashour <kylebshr@me.com>
2024-03-05 10:11:43 -0800

    Remove issuers

--- Sources/HakoShared/Models/Issuer.swift
+++ Sources/HakoShared/Models/Issuer.swift
@@ -24,25 +24,3 @@ public struct Issuer: Identifiable, Hashable, Equatable, Codable {
         self.name = name
     }
 }
-
-extension Issuer {
-    static let chase = Issuer(
-        id: ID(rawValue: "chase"),
-        name: "Chase"
-    )
-
-    static let amex = Issuer(
-        id: ID(rawValue: "amex"),
-        name: "American Express"
-    )
-
-    static let apple = Issuer(
-        id: ID(rawValue: "apple"),
-        name: "Apple"
-    )
-
-    static let citi = Issuer(
-        id: ID(rawValue: "citi"),
-        name: "Citi"
-    )
-}
